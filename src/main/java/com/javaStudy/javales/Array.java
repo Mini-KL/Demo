aa842c5c88ec8bf25c41b43e8137c87e9906d6b6
1026683139_qq.com_blockcc <123456Qq#>
2019-12-12 21:05:13 +0800

    第一次提交

---
+++ src/main/java/com/javaStudy/javales/Array.java
@@ -0,0 +1,14 @@
+package com.javaStudy.javales;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class Array {
+    @Test
+    public void demo1(){
+        String[] arr = new String[]{"1","2"};
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
